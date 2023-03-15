9464ca717e0eeb998e1abde7d30e88dd25a92fec
hackek <hackett.kd@gmail.com>
2023-03-15 14:40:24 -0600

    MVP is complete according to assignment specifications. Submitting final product now.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 const app = express();
 const port = 8080;
 const myIdData = [1, 2, 3];
@@ -10,28 +11,28 @@ app.get('/', (req, res) => {
     sameSite: 'strict',
     secret: 'somesuperimpressivestringofcharactersnoonewouldpossiblyguessmaybe'
   };
-  res.cookie('another_name', 'neato_value', opts);
-  res.status(200).send('Hello World!');
+  res.cookie('session', 'neato_value', opts);;
+  res.status(200).send(`Navigate to /login?name=YOUR_NAME`);
 
+})
+
+app.use(cookieParser());
+
+app.get('/login', (req, res) => {
+  let name = req.query.name;
+  var opts = {
+    maxAge: 900000,
+    httpOnly: true,
+    sameSite: 'strict'
+  };
+
+  res.cookie('name', name, opts);
+  res.status(200).send(`You're logged in! Navigate to /hello`)
+})
+
+app.get('/hello', (req, res) => {
+  let name = req.cookies.name;
+  res.send(`Welcome ${name}`);
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-// // set up cookie session middleware
-// app.use(cookieSession({
-//   name: 'user_session',
-//   httpOnly: true,
-//   sameSite: 'strict',
-//   secret: 'somesuperimpressivestringofcharactersnoonewouldpossiblyguessmaybe'
-// }))
-
-app.get('/login', (req, res, next) => {
-  // Gets the username of the current session via req.session.username
-  // // check credentials
-  // if (loginSuccess(req.body.username, req.body.password)) {
-  //   // set username to the session
-  //   req.session.username = req.body.username;
-  // }
-  console.log(`You're logged in ${req.session.username}!`)
-  res.status(200).send(`You're logged in ${req.session.username}!`)
-})
